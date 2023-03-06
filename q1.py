ce7642c12b55c35a5a2718bc445f93805177417d
SACHIN <119405675+sachin-fullstack@users.noreply.github.com>
2023-03-06 21:39:08 +0530

    Create q1.py

---
+++ q1.py
@@ -0,0 +1,9 @@
+# CTF Question: Decrypt a message
+
+# The following message was encrypted using a simple substitution cipher
+# In this cipher, each letter in the original message is replaced with a different letter
+# For example, the letter 'a' might be replaced with 'q', the letter 'b' with 'z', and so on
+
+# Your task is to decrypt the message below and determine the original message
+
+encrypted_message = "dnh rqh iurp wkh frqyhuvlrq ri wkh edfnjurxqg"
